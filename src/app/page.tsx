81352b5477cd358e6c121020682514eba6f678e8
panditdhamdhere <dhamdherepandit@gmail.com>
2024-06-11 19:09:11 +0530

    wallet done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,93 @@
-import Image from "next/image";
+"use client";
+
+import { createThirdwebClient, getContract } from "thirdweb";
+import {
+  ConnectButton,
+  TransactionButton,
+  useActiveAccount,
+} from "thirdweb/react";
+import { createWallet } from "thirdweb/wallets";
+import { claimTo } from "thirdweb/extensions/erc1155";
+import { useSendCalls } from "thirdweb/wallets/eip5792";
+
+// Only certain chains supported: https://www.smartwallet.dev/FAQ#what-networks-are-supported
+// NOTE: To use mainnet chains, you need to add a credit card to your thirdweb account.
+// To use testnets, such as baseSepolia, no card is required.
+
+import { arbitrum } from "thirdweb/chains";
+const chainToUse = arbitrum;
+
+const thirdwebClient = createThirdwebClient({
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!,
+});
 
 export default function Home() {
+  const account = useActiveAccount();
+
+  // This is so we can use a paymaster service to pay for the transaction gas fees
+  const { mutateAsync: sendCalls } = useSendCalls({
+    client: thirdwebClient,
+    waitForResult: true,
+  });
+
+  // Function to send the claim NFT transaction with gas fees covered.
+  async function sendSponsoredTransaction() {
+    const claimTx = claimTo({
+      // contract using a chain + contract address combo
+      contract: getContract({
+        client: thirdwebClient,
+        chain: chainToUse,
+        address: "0x1093C2Bd8437664bd523A7a86AE5825c8e274Da5",
+      }),
+      quantity: BigInt(1), 
+      to: account?.address!,
+      tokenId: BigInt(0),
+    });
+
+    // Send the transaction with the paymaster service
+    return await sendCalls({
+      calls: [claimTx], // The claim transaction. We could put multiple transactions here in theory.
+      capabilities: {
+        paymasterService: {
+          // Docs: https://portal.thirdweb.com/connect/account-abstraction/infrastructure
+          url: `https://${chainToUse.id}.bundler.thirdweb.com/${thirdwebClient.clientId}`,
+        },
+      },
+    });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="bg-blue-400 flex flex-col items-center justify-center h-screen">
+      <ConnectButton
+        client={thirdwebClient}
+        // The array of wallets we want to show to users, we just provide 1 - Coinbase Wallet
+        wallets={[
+          // Use com.coinbase.wallet for Coinbase Wallet
+          createWallet("com.coinbase.wallet", {
+            walletConfig: {
+              // Specify we do not want coinbase wallet browser extension support, just smart wallet
+              options: "smartWalletOnly",
+            },
+            // What chains we want to support in our app
+            chains: [chainToUse],
+            // This is the metadata that shows up when prompting the user to connect their wallet
+            appMetadata: {
+              logoUrl: `https://github.com/jarrodwatts/polygon-coinbase-smart-wallet/blob/main/public/wizard-hat.png?raw=true`,
+              name: "Blockchain through Coinbase Smart Wallet by pandit",
+              description: "Mint NFTs on Polygon using Coinbase Smart Wallet",
+            },
+          }),
+        ]}
+      />
+      {/* <TransactionButton
+        transaction={() =>
+          // @ts-expect-error: Just a type issue, can probably be fixed easily
+          sendSponsoredTransaction()
+        }
+        payModal={false}
+      >
+        CoinBASE
+      </TransactionButton> */}
+    </div>
   );
 }
